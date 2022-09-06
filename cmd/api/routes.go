950e5e5c4129cb10e342b1b30a84c561c6f78f74
Desire Amagwula <dezamagwula@gmail.com>
2022-09-05 21:52:08 -0500

    added routes file

---
+++ cmd/api/routes.go
@@ -0,0 +1,19 @@
+// Filename
+
+package main 
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+
+func (app *application) routes() *httprouter.Router {
+	// Create
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/schools", app.CreateSchoolHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/schools/:id", app.showSchoolHandler)
+
+	return router
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module appletree.desireamagwula.net
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
