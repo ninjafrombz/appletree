6eaff2e2c972c15d2e9ccfd2f1bec553e318e68a
Desire Amagwula <dezamagwula@gmail.com>
2022-10-21 16:42:40 -0600

    updated the updateSchoolHandler() to handle partial update

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,7 +16,7 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/schools", app.createSchoolHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/schools/:id", app.showSchoolHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/schools/:id", app.updateSchoolHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/schools/:id", app.updateSchoolHandler)
     router.HandlerFunc(http.MethodDelete, "/v1/schools/:id", app.deleteSchoolHandler)
 
 	return router
--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -101,16 +101,16 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	// This method does a complete replacement 
-	// Get the ID for the school that needs updating 
-
+	// This method does a partial replacement
+	// Get the id for the school that needs updating
 	id, err := app.readIDParam(r)
 	if err != nil {
 		app.notFoundResponse(w, r)
 		return
 	}
-	// Fetch the original record from the database
+	// Fetch the orginal record from the database
 	school, err := app.models.Schools.Get(id)
+	// Handle errors
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
@@ -118,18 +118,22 @@ func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Reque
 		default:
 			app.serverErrorResponse(w, r, err)
 		}
-
 		return
 	}
-	// Create an input struct to hold data read in from the client 
+
+	
+	// Create an input struct to hold data read in from the client
+	// We update input struct to use pointers because pointers have a
+	// default value of nil
+	// If a field remains nil then we know that the client did not update it
 	var input struct {
-		Name    string   `json:"name"`
-		Level   string   `json:"level"`
-		Contact string   `json:"contact"`
-		Phone   string   `json:"phone"`
-		Email   string   `json:"email"`
-		Website string   `json:"website"`
-		Address string   `json:"address"`
+		Name    *string  `json:"name"`
+		Level   *string  `json:"level"`
+		Contact *string  `json:"contact"`
+		Phone   *string  `json:"phone"`
+		Email   *string  `json:"email"`
+		Website *string  `json:"website"`
+		Address *string  `json:"address"`
 		Mode    []string `json:"mode"`
 	}
 
@@ -139,19 +143,35 @@ func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Reque
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	// Copy / UPdate the fields or values in the school variable using the fields 
-	// in the input struct
+	// Check for updates
+	if input.Name != nil {
+		school.Name = *input.Name
+	}
+	if input.Level != nil {
+		school.Level = *input.Level
+	}
+	if input.Contact != nil {
+		school.Contact = *input.Contact
+	}
+	if input.Phone != nil {
+		school.Phone = *input.Phone
+	}
+	if input.Email != nil {
+		school.Email = *input.Email
+	}
+	if input.Website != nil {
+		school.Website = *input.Website
+	}
+	if input.Address != nil {
+		school.Address = *input.Address
+	}
+	if input.Mode != nil {
+		school.Mode = input.Mode
+	}
 
-	school.Name = input.Name
-	school.Level = input.Level
-	school.Contact = input.Contact
-	school.Phone = input.Phone
-	school.Email = input.Email
-	school.Website = input.Website
-	school.Address = input.Address
-	school.Mode = input.Mode
-	// Perform some validation on the updated school. If validation fails then we send 
-	// a 422 - unprocessable entity response to the client
+	// Perform validation on the updated School. If validation fails, then
+	// we send a 422 - Unprocessable Entity respose to the client
+	// Initialize a new Validator instance
 	v := validator.New()
 
 	// Check the map to determine if there were any validation errors
